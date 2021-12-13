029c9c52cd2b59d38fba2678192efaf28373edf8
FannyS <fanny.scheltgen@gmail.com>
2021-12-13 11:02:12 +0100

    ajout fichiers exo string et num

---
+++ num.py
@@ -0,0 +1,20 @@
+
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print(3 + 2)
+print(4 - 3)
+print(3 * 5)
+print(4 / 2)
+print(3 ** 2)
+
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+print(0.2 + 0.1)
+#this is a comment
+print(4 / 2)
---
+++ string.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 13 10:51:19 2021
+
+@author: Peridot
+"""
+
+quote = "Yoda said 'Do or do not, there is no try'"
+print(quote)
+
+name = "Fanny"
+print(name.lower())
+print(name.title())
+print(name.upper())
+
+first_name = "Fanny"
+last_name = "Scheltgen"
+print(first_name + " " + last_name)
+
+whitespace_name = " Fanny "
+print(whitespace_name)
+print(whitespace_name.lstrip())
+print(whitespace_name.rstrip())
+print(whitespace_name.strip())
+
